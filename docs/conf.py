6fa0e0e09dace78b90f4a3c86fe2d6920bac33bd
Desmond Lin <dezlinyiming@gmail.com>
2025-05-13 17:06:44 +0100

    s

---
+++ docs/conf.py
@@ -0,0 +1,48 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'EXODIA'
+copyright = '2025, DEZLIN'
+author = 'DEZLIN'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+  'sphinx.ext.napoleon',
+  'sphinx.ext.autodoc',
+  'myst_parser'
+]
+
+source_suffix = {
+  '.rst': 'restructuredtext',
+  '.txt': 'markdown',
+  '.md': 'markdown',
+}
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_book_theme'
+html_title = 'Contact Mosaic documentation'
+html_theme_options = {
+  'repository_url': 'https://github.com/ShozenD/high_res_brc',
+  'use_repository_button': True
+}
+html_static_path = ['_static']
+
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path('../../').resolve()))
